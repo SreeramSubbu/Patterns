ad5ed84ad5be98e07c82da4a5f595cac5880c378
SreeramSubbu <sreeramsiyer@gmail.com>
2017-10-23 20:55:34 +0100

    Base Version   Observer and Decorator pattern

---
+++ ScratchBook/src/pattern/decorator/CinnamonCoffee.java
@@ -0,0 +1,24 @@
+package pattern.decorator;
+
+/**
+ *
+ * @author ramudu
+ */
+public class CinnamonCoffee extends FlavouredCoffee {
+
+    public CinnamonCoffee(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return "Cinnamon " + this.coffee.getDescription();
+    }
+
+    @Override
+    public Double getCost() {
+
+        return this.coffee.getCost() + 1.00;
+    }
+
+}
---
+++ ScratchBook/src/pattern/decorator/Coffee.java
@@ -0,0 +1,39 @@
+package pattern.decorator;
+
+/**
+ *
+ * @author ramudu
+ */
+public abstract class Coffee {
+
+    private String description;
+    private Double cost;
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the cost
+     */
+    public Double getCost() {
+        return cost;
+    }
+
+    /**
+     * @param cost the cost to set
+     */
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+}
